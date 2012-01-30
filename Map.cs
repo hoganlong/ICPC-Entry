8d33b41f31311f5b15dd6eb3d4175706ffd3639b
hoganlong <hoganlong@gmail.com>
2012-01-29 21:28:57 -0500

    Map enhancements
    Region object (combined color and vertex lists)
    Region and Marker now have color change event.

--- Map.cs
+++ Map.cs
@@ -21,16 +21,28 @@ public class Map
 	// Color value for unclaimed pucks.
 	public const int GREY = 2;
 
+	// Color unknown
+	public const int UNKNOWN = -1;
+
 
 	internal List<int> candidates = new List<int>();
 	internal Vertex3D[] vertexList=null;
 	internal Vector2D[] vertexPoints=null;
 	internal Marker[] mList=null;
-	internal int[][] regionList=null;
-	internal int[] regionColors=null;
 	internal Pusher[] pList = null;
 	internal int[] vertexColors = null;
-	
+
+	internal Region[] regionListNew = null;
+
+	// Internal change events (we could use an event model, but I'm lazy.
+	internal void RegionColorChanged(int regionNumber, int oldColor, int newColor)
+	{
+	}
+
+	internal void MarkerColorChanged(int markerNumber, int oldColor, int newColor)
+	{
+	}
+
 	// Runs once before first turn
 	internal void ReadStatic()
 	{
@@ -51,19 +63,20 @@ public class Map
 		// Read the list of region outlines.
 		n = int.Parse(Console.ReadLine());
 		// List of regions in the map
-		regionList = new int[n][];
+		regionListNew = new Region[n];
 		for (int i = 0; i < n; i++)
 		{
 			string[] tokens = Console.ReadLine().Split();
 			int m = int.Parse(tokens[0]);
-			regionList[i] = new int[m];
+			regionListNew[i] = new Region();
+      		regionListNew[i].vertexList = new int[m];
 			for (int j = 0; j < m; j++)
-				regionList[i][j] = int.Parse(tokens[j + 1]);
+				regionListNew[i].vertexList[j] = int.Parse(tokens[j+1]);
 		}
 
 		// List of current region colors, pusher and marker locations.
 		// These are updated on every turn snapshot from the game.
-		regionColors = new int[regionList.Length];
+		//regionColors = new int[regionList.Length];
 		pList = new Pusher[2 * PCOUNT];
 		for (int i = 0; i < pList.Length; i++)
 			pList[i] = new Pusher();
@@ -83,8 +96,15 @@ public class Map
 		// Read all the region colors.
 		tokens = Console.ReadLine().Split();
 		int n = int.Parse(tokens[0]);
-		for (int i = 0; i < regionList.Length; i++)
-			regionColors[i] = int.Parse(tokens[i + 1]);
+		for (int i = 0; i < regionListNew.Length; i++)
+		{
+			int c = int.Parse(tokens[i + 1]);
+			if (regionListNew[i].color != c)
+			{
+				RegionColorChanged(i, regionListNew[i].color, c);
+				regionListNew[i].color = c;
+			}
+		}
 
 		// Read all the pusher locations.
 		n = int.Parse(Console.ReadLine());
@@ -106,22 +126,26 @@ public class Map
 			mList[i].pos.y = double.Parse(tokens[1]);
 			mList[i].vel.x = double.Parse(tokens[2]);
 			mList[i].vel.y = double.Parse(tokens[3]);
-			mList[i].color = int.Parse(tokens[4]);
+			int c = int.Parse(tokens[4]);
+			if (mList[i].color != c)
+			{
+				MarkerColorChanged(i, mList[i].color, c);
+				mList[i].color = c;
+			}
 		}
-
 	}
 
 	internal void StartTurnWork()
 	{
 		// Compute a bit vector for the region colors incident on each vertex.
 		vertexColors = new int[vertexList.Length];
-		for (int i = 0; i < regionList.Length; i++)
-			for (int j = 0; j < regionList[i].Length; j++)
+		for (int regionNumber = 0; regionNumber < regionListNew.Length; regionNumber++)
+			for (int rVertextNumber = 0; rVertextNumber < regionListNew[regionNumber].vertexList.Length; rVertextNumber++)
 			{
-				if (regionColors[i] == 0)
-					vertexColors[regionList[i][j]] |= 1;
+				if (regionListNew[regionNumber].color == 0)
+					vertexColors[regionListNew[regionNumber].vertexList[rVertextNumber]] |= 1;
 				else
-					vertexColors[regionList[i][j]] |= (1 << regionColors[i]);
+					vertexColors[regionListNew[regionNumber].vertexList[rVertextNumber]] |= (1 << regionListNew[regionNumber].color);
 			}
 		// Candidate vertices for putting a marker on, vertices that have
 		// some red but are not all red.
